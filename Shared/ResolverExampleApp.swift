9a91f57fd794a89b797d0623b6dc2700196ff26e
Andrew Brehm <97919323+AndrewBrehmAtDialexa@users.noreply.github.com>
2022-01-24 14:34:02 -0500

    initial commit

---
+++ Shared/ResolverExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResolverExampleApp.swift
+//  Shared
+//
+//  Created by Andrew Brehm on 1/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ResolverExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
